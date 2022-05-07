259c3c3e6c8c93134108907b0e15d2f745ea4aec
Bereket T <contact.bereket.terefe@gmail.com>
2022-05-07 17:39:23 +0300

    [init|fix|polish] - finished except for sample data

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,4 +53,13 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
+sr.reveal('.scroll_for_more',{interval: 200});
+
+/*===== AUTO CAROUSEL =====*/
+counter = 1
+// setInterval(() => {
+//     document.getElementById("radio" + counter).checked = true
+//     counter ++
+//     if(counter > 5)  counter = 1
+// }, 7000)

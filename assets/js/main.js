6bb4124c14f3fa6493c02ee80c1b5818277c0ef9
Bereket T <contact.bereket.terefe@gmail.com>
2022-05-12 19:30:27 +0300

    [FIX] - minor error fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,12 +30,12 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        const sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']')?.classList.add('active')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']')?.classList.remove('active')
         }
     })
 }
